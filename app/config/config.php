17022891dd4b8e60fe634f446986d9611be4104f
moisesmdpf <moises_daniel_@outlook.com>
2018-04-02 11:24:21 -0400

    Agregado datos del fichero de configuracion

---
+++ app/config/config.php
@@ -0,0 +1,12 @@
+<?php
+
+    /**
+     * Archivo de configuracion y definicion de constantes generales
+     * de la aplicacion php. Cualquier configuracion necesaria para el funcionamiento
+     * y adecuación de un proyecto a la logica de negocio debe realizarse mediante este archivo.
+     */
+
+    define('APP_PATH',dirname(dirname(__FILE__)));      // Ruta principal de los archivos de aplicacion
+    define('URL_PATH','_URL');                          // URL principal del sitio web (Se configura)
+    define('SITE_NAME','_SITENAME_');                   // Nombre del sitio web
+?>
