15a099e051f7aeb01cdd513a873026a0501a2f06
Moises Piñate <moises_daniel_@outlook.com>
2018-04-08 12:33:56 -0400

    Configurando ruta raiz de la aplicacion

--- app/config/config.php
+++ app/config/config.php
@@ -6,7 +6,7 @@
      * y adecuación de un proyecto a la logica de negocio debe realizarse mediante este archivo.
      */
 
-    define('APP_PATH',dirname(dirname(__FILE__)));      // Ruta principal de los archivos de aplicacion
-    define('URL_PATH','_URL');                          // URL principal del sitio web (Se configura)
-    define('SITE_NAME','_SITENAME_');                   // Nombre del sitio web
+    define('APP_PATH',dirname(dirname(__FILE__)));              // Ruta principal de los archivos de aplicacion
+    define('ROOT_PATH','/projects/github/mvc/mvc-framework/');  // URL principal del sitio web (Se configura)
+    define('SITE_NAME','_SITENAME_');                           // Nombre del sitio web (Se configura)
 ?>
