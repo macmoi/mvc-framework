53333a5543af6546ef8731f2f89d1df6b78018a0
Moises Piñate <moises_daniel_@outlook.com>
2018-04-09 04:43:40 -0400

    Agregando datos de configuracion de BD

--- app/config/config.php
+++ app/config/config.php
@@ -6,7 +6,14 @@
      * y adecuación de un proyecto a la logica de negocio debe realizarse mediante este archivo.
      */
 
+    //CONFIGURACIONES DE LA APLICACION
     define('APP_PATH',dirname(dirname(__FILE__)));              // Ruta principal de los archivos de aplicacion
     define('ROOT_PATH','/projects/github/mvc/mvc-framework/');  // URL principal del sitio web (Se configura)
     define('SITE_NAME','_SITENAME_');                           // Nombre del sitio web (Se configura)
+
+    //CONFIGURACIONES DE BASE DE DATOS
+    define('DB_HOST','localhost');                              // Host de bdd
+    define('DB_USER','root');                                   // Usuario de acceso
+    define('DB_PASS','123456789');                                // Contraseña de acceso
+    define('DB_NAME','dbname');                                 // Nombre de BDD
 ?>
